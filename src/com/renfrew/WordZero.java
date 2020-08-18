4d152022127d761e08a4e899afa49a3c48cdddac
liang Chen <Renfrew_world@hotmail.com>
2020-08-18 00:11:51 -0400

    Word0 is completed

--- src/com/renfrew/WordZero.java
+++ src/com/renfrew/WordZero.java
@@ -1,27 +1,23 @@
 package com.renfrew;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class WordZero {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        System.out.println(word0(new String[] {"a", "b", "a", "b"}));
+        System.out.println(word0(new String[] {"a", "b", "a", "c", "b"}));
+        System.out.println(word0(new String[] {"c", "b", "a"}));
         System.out.println();
     }
 
-    public static int interpret(int value, String[] commands, int[] args) {
-        for (int i = 0; i < commands.length; ++i) {
-            switch (commands[i]) {
-                case "+":
-                    value += args[i];
-                    break;
-                case "-":
-                    value -= args[i];
-                    break;
-                case "*":
-                    value *= args[i];
-                    break;
-                default:
-                    return -1;
-            }
+    public static Map<String, Integer> word0(final String[] strings) {
+        Map<String, Integer> ret = new HashMap<>();
+
+        for (final String string : strings) {
+            ret.put(string, 0);
         }
-        return value;
+
+        return ret;
     }
 }
