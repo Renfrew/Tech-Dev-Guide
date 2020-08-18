bbe39e373a5e6f16b3f4622bc0832d26ac740775
liang Chen <Renfrew_world@hotmail.com>
2020-08-18 17:25:07 -0400

    MineSweeper now can hide and show the board properly

--- src/com/renfrew/Minesweeper.java
+++ src/com/renfrew/Minesweeper.java
@@ -17,6 +17,11 @@ public class Minesweeper {
 
     private static final int CELL_ERROR = -1;
     private static final int CELL_MINE = 9;
+    enum CellStatus {
+        HIDDEN,
+        SHOW,
+        FLAGED,
+    }
 
     int steps; // Steps that the player have taken.
     Date date; // Time that the player start the game
@@ -86,7 +91,7 @@ public class Minesweeper {
         private int rowNumber;
         private int colNumber;
 
-        private int[][] matrix;
+        private Cell[][] matrix;
 
         Matrix(int rows, int cols) {
             rowNumber = rows;
@@ -95,7 +100,12 @@ public class Minesweeper {
         }
 
         void init() {
-            matrix = new int[rowNumber][colNumber];
+            matrix = new Cell[rowNumber][colNumber];
+            for (Cell[] row : matrix) {
+                for (int col = 0; col < row.length; ++col) {
+                    row[col] = new Cell();
+                }
+            }
         }
 
         void resize(int rows, int cols) {
@@ -106,7 +116,7 @@ public class Minesweeper {
 
         int at(int row, int col) {
             if (0 <= row && row < rowNumber && 0 <= col && col < colNumber) {
-                return matrix[row][col];
+                return matrix[row][col].value;
             } else {
                 return CELL_ERROR;
             }
@@ -114,7 +124,7 @@ public class Minesweeper {
 
         void set(int row, int col, int value) {
             if (0 <= row && row < rowNumber && 0 <= col && col < colNumber) {
-                matrix[row][col] = value;
+                matrix[row][col].value = value;
             }
         }
 
@@ -123,9 +133,9 @@ public class Minesweeper {
         void addOne(int row, int col) {
             if (0 <= row && row < rowNumber
              && 0 <= col && col < colNumber
-             && matrix[row][col] != CELL_MINE) {
+             && matrix[row][col].value != CELL_MINE) {
 
-                matrix[row][col] += 1;
+                matrix[row][col].value += 1;
             }
         }
 
@@ -138,14 +148,32 @@ public class Minesweeper {
         }
 
         void printMatrix() {
-            for (int[] row : matrix) {
-                for (int i : row) {
-                    System.out.print(i);
+            for (Cell[] row : matrix) {
+                for (Cell i : row) {
+                    switch (i.status) {
+                        case HIDDEN:
+                            System.out.print('-');
+                            break;
+                        case SHOW:
+                            if (i.value == 0) {
+                                System.out.print(" ");
+                            } else {
+                                System.out.print(i.value);
+                            }
+                            break;
+                        case FLAGED:
+                            System.out.print('*');
+                    }
                     System.out.print(' ');
                 }
 
                 System.out.println();
             }
         }
+    }
+
+    class Cell {
+        int value = 0;
+        CellStatus status = CellStatus.HIDDEN;
     }
 }
